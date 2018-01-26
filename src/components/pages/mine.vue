813751122ac27095da1f1e6cb8bb132a2df304d1
TeanLee <ltt598625763@163.com>
2018-01-26 21:30:12 +0800

    mine页面添加了头部、头像信息

--- src/components/pages/mine.vue
+++ src/components/pages/mine.vue
@@ -4,6 +4,13 @@
         <mt-button icon="back" slot="left"></mt-button>
         <mt-button icon="more" slot="right"></mt-button>
     </mt-header>
+    <div class="info-card">
+        <div class="avater">
+            <img src="../../assets/images/avatar.jpg" alt="">
+            <a>TeanLee</a>
+        </div>
+    </div>
+    <div class="order"></div>
   </div>
 </template>
 
@@ -13,6 +20,28 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.info-card {
+    width: 90%;
+    margin: 50px auto;
+    height: 100px;
+    border: 1px solid red;
+}
+.avater img {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    float: left;
+}
+.avater {
+    float: left;
+    line-height: 50px;
+    text-decoration: none;
+    color: #26a2ff;
+}
+.order {
+    width: 100%;
+    height: 80px;
+    outline: 1px solid blue;
+}
 </style>
