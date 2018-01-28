01459d9b5d035393ed1097f668bac1037039120c
TeanLee <ltt598625763@163.com>
2018-01-28 19:14:04 +0800

    实现了首页分类栏

--- src/components/pages/home.vue
+++ src/components/pages/home.vue
@@ -2,15 +2,18 @@
   <div class="home">
       <mt-search class="search" cancel-text="取消" placeholder="搜索"></mt-search>
       <hm-swipper></hm-swipper>
+      <home-sort></home-sort>
   </div>
 </template>
 
 <script>
 import Swipper from '@/components/common/swipper'
+import HomeSort from '@/components/common/home-sort'
 
 export default {
     components: {
-        'hm-swipper': Swipper
+        'hm-swipper': Swipper,
+        'home-sort': HomeSort
     }
 }
 </script>
