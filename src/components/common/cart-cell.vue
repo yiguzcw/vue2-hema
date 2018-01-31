0e2e83d74ddf2a695c58c2d49cd52b9a17ae91bb
TeanLee <ltt598625763@163.com>
2018-01-31 11:48:38 +0800

    购物车界面

--- src/components/common/cart-cell.vue
+++ src/components/common/cart-cell.vue
@@ -7,8 +7,8 @@
             </div>
             <div class="content left">
                 <img src="../../assets/images/avatar.jpg" alt="">
-                <p>超级</p>
-                <p>￥9999</p>
+                <p class="title">超级超级超级超级超级超级超级超级超级超级超级</p>
+                <p class="price red">￥9999</p>
             </div>
             <div class="num left">
                 <ul>
@@ -19,6 +19,40 @@
             </div>
 
         </div>
+
+        <div class="cell">
+
+            <div class="icon left">
+                <img src="../../assets/icons/good-sel.png" alt="">
+            </div>
+            <div class="content left">
+                <img src="../../assets/images/avatar.jpg" alt="">
+                <p class="title">超级超级超级超级超级超级超级超级超级超级超级</p>
+                <p class="price red">￥9999</p>
+            </div>
+            <div class="num left">
+                <ul>
+                    <li>-</li>
+                    <li>12</li>
+                    <li>+</li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="CartCount">
+            <div class="icon">
+                <img src="../../assets/icons/good-sel.png" alt="">
+            </div>
+            <div class="content lh44">
+                <span class="left">全选</span>
+                <span class="right red">￥9999</span>
+            </div>
+            <div class="num">
+                <mt-button type="primary">结算</mt-button>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -34,13 +68,13 @@
 }
 .cell {
     width: 100%;
-    height: 100px;
+    height: 80px;
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
     flex-basis: auto;
-    margin-left: -10px;
+    margin-bottom: 15px;
 }
 ul {
     margin: 0;
@@ -60,32 +94,59 @@ li {
 }
 .icon {
     width: 8%;
-    outline: 1px solid red;
 }
 img {
     width: 100%;
     height: 100%;
 }
 .content {
-    width: 70%;
+    width: 65%;
     height: 100%;
-    outline: 1px solid black;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+.content > p {
+    font-size: 14px;
+    text-align: left;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
 }
 .content > img {
-    width: 45%;
+    width: 38%;
     height: 100%;
     float: left;
 }
-.content > p {
-    text-align: left;
-}
 .num {
-    width: 20%;
+    width: 27%;
     height: 100%;
-    outline: 1px solid blue;
     position: relative;
 }
 .left {
     float: left;
+}
+.title {
+    
+}
+.CartCount {
+    position: fixed;
+    margin-left: -15px;
+    bottom: 52px;
+    width: 100%;
+    height: 44px;
+    border-top: 1px solid #999;
+    display: flex;
+    padding-left: 20px;
+    flex-direction: row;
+    align-items: center;
+}
+.lh44 {
+    line-height: 44px;
+    padding: 10px;
+}
+.red {
+    color: #fe4544;
+    font-size: 22px;
 }
 </style>
